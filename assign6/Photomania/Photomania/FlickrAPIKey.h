35b8198beb69c38c3e121715e3e05c274da1608c
Pat Boyle <patboyle@gmail.com>
2012-11-22 09:25:00 -0800

    add assign6

---
+++ assign6/Photomania/Photomania/FlickrAPIKey.h
@@ -0,0 +1,11 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"151f71443afd3cad82a336b25245cf0f"
